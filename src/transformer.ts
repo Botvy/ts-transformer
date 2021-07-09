ff0da963a406251e821c83c0fa220a0830ae4247
Yannick Fricke <yannickfricke@googlemail.com>
2021-07-09 19:12:55 +0200

    Updated the transformer

--- src/transformer.ts
+++ src/transformer.ts
@@ -1,53 +1,117 @@
 import * as ts from "typescript";
+import { ImportDeclaration, ImportSpecifier } from "typescript";
+
+interface ImportStatement {
+    name: string;
+    alias?: string;
+}
 
 const transformerProgram = (program: ts.Program) => {
-  const typeChecker = program.getTypeChecker();
+    const transformerFactory: ts.TransformerFactory<ts.SourceFile> = (
+        context
+    ) => {
+        return (sourceFile) => {
+            const visitor = (node: ts.Node): ts.Node => {
+                if (!sourceFile.fileName.includes("example2.ts")) {
+                    return node;
+                }
 
-  // Create array of found symbols
-  const foundSymbols = new Array<ts.Symbol>();
+                if (ts.isSourceFile(node)) {
+                    return ts.visitEachChild(node, visitor, context);
+                }
 
-  const transformerFactory: ts.TransformerFactory<ts.SourceFile> = (
-    context
-  ) => {
-    return (sourceFile) => {
-      const visitor = (node: ts.Node): ts.Node => {
-        if (ts.isIdentifier(node)) {
-          const relatedSymbol = typeChecker.getSymbolAtLocation(node);
+                console.log({ node, type: ts.SyntaxKind[node.kind] });
 
-          // Check if array already contains same symbol - check by reference
-          if (
-            foundSymbols.some((foundSymbol) => foundSymbol === relatedSymbol)
-          ) {
-            const foundIndex = foundSymbols.indexOf(relatedSymbol!);
-            console.log(
-              `Found existing symbol at position = ${foundIndex} and name = "${
-                relatedSymbol?.name ?? ""
-              }"`
-            );
-          } else {
-            // If not found, Add it to array
-            foundSymbols.push(relatedSymbol);
+                if (!is_import_declaration(node)) {
+                    return ts.visitEachChild(node, visitor, context);
+                }
 
-            console.log(
-              `Found new symbol with name ${
-                ts.SyntaxKind[relatedSymbol.declarations[0].kind]
-              } = "${relatedSymbol?.name ?? ""}". Added at positon = ${
-                foundSymbols.length - 1
-              }`
-            );
-          }
+                const importedModule = (
+                    node.moduleSpecifier as ts.StringLiteral
+                ).text;
 
-          return node;
-        }
+                if (!ts.isSourceFile(node.parent)) {
+                    return node;
+                }
 
-        return ts.visitEachChild(node, visitor, context);
-      };
+                if (importedModule.toLowerCase() !== "react") {
+                    return node;
+                }
 
-      return ts.visitNode(sourceFile, visitor);
+                let namedDefaultImport = node.importClause?.name?.getText();
+                const namedImports: ImportStatement[] = [];
+
+                node.importClause?.namedBindings?.forEachChild((childNode) => {
+                    const text = childNode.getText();
+
+                    if (is_import_specifier(childNode)) {
+                        namedImports.push({
+                            name:
+                                childNode.propertyName?.getText() === undefined
+                                    ? childNode.name.getText()
+                                    : childNode.propertyName?.getText(),
+                            alias:
+                                childNode.propertyName?.getText() !== undefined
+                                    ? childNode.name.getText()
+                                    : undefined,
+                        });
+                    } else {
+                        if (node.getText().includes("* as")) {
+                            namedDefaultImport = text;
+                        } else {
+                            namedImports.push({
+                                name: text,
+                            });
+                        }
+                    }
+                });
+
+                console.log({ namedDefaultImport, namedImports });
+
+                if (
+                    namedDefaultImport === undefined &&
+                    namedImports.length > 0
+                ) {
+                    return ts.factory.createVariableDeclarationList([
+                        ts.factory.createVariableDeclaration(
+                            "LOL",
+                            undefined,
+                            undefined,
+                            ts.factory.createIdentifier("test")
+                        ),
+                    ]);
+                } else if (
+                    namedDefaultImport !== undefined &&
+                    namedImports.length === 0
+                ) {
+                    return ts.factory.createIdentifier(
+                        "Found default import only"
+                    );
+                } else if (
+                    namedDefaultImport !== undefined &&
+                    namedImports.length > 0
+                ) {
+                    return ts.factory.createIdentifier(
+                        "Found default and named imports"
+                    );
+                }
+
+                return ts.visitNode(sourceFile, visitor);
+            };
+
+            return ts.visitNode(sourceFile, visitor);
+        };
     };
-  };
 
-  return transformerFactory;
+    return transformerFactory;
 };
+
+function is_import_declaration(node: ts.Node): node is ImportDeclaration {
+    return ts.isImportDeclaration(node);
+}
+
+function is_import_specifier(node: ts.Node): node is ImportSpecifier {
+    return ts.isImportSpecifier(node);
+}
 
 export default transformerProgram;
